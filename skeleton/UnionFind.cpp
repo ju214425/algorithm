03f7c94b4e92ff711d18fd833cc7d41fc09db7c2
2016025841 <2016025841@hanyang.ac.kr>
2019-07-20 22:01:33 +0900

    UnionFind

---
+++ skeleton/UnionFind.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+struct UnionFind{
+	ll size;
+	vector<ll> parent;
+	vector<ll> level;
+
+	UnionFind(ll size){
+		parent.resize(size+1);
+		level.resize(size+1);
+	}
+
+	void init(){
+		for(ll i = 1 ; i <= size ; i++){
+			parent[i] = i;
+			level[i] = 1;
+		}
+	}
+
+	ll find(ll x){
+		if(parent[x] == x)
+			return x;	
+		return parent[x] = find(parent[x]);
+	}
+
+	void join(ll x, ll y){
+		x = find(x);
+		y = find(y);
+		if(x == y) 
+			return;
+		if(level[x] > level[y]) 
+			swap(x, y);
+
+		parent[x] = y;
+
+		if(level[x] == level[y])
+			++level[v];
+
+	}
+};
