7e528dfed6dbc2472256200c43c825e877675d45
ju214425 <ju214425@naver.com>
2023-04-17 20:17:13 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+arr = [i for i in range(n+1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+
+    arr[i], arr[j] = arr[j], arr[i]
+
+for i in range(1, len(arr)):
+    print(arr[i], end = ' ')
