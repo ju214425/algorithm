9891686ed5becde79ad287d108c941c0c8a53fad
ju214425 <ju214425@naver.com>
2023-05-04 20:32:16 +0900

    [level 2] Title: 모음 사전, Time: 2.66 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,30 @@
+w = ['A', 'E', 'I', 'O', 'U']
+
+def function(arr, limit, cur, word):
+    if limit == cur:
+        arr.add(word)
+        return
+    
+    for i in range(5):
+        word = word + w[i]
+        function(arr, limit, cur+1, word)
+        word = word[:-1]
+
+def solution(word):
+    answer = 0
+    
+    arr = set()
+    
+    for i in range(1, 6):
+        function(arr, i, 0, '')
+    
+    # arr.sort()
+    
+    arr = sorted(list(arr))
+    
+    for idx, value in enumerate(arr):
+        if value == word:
+            answer = idx+1
+            break
+    
+    return answer
