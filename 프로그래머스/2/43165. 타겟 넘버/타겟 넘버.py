33ebbb9c9fff97543fd6eddce261f253ccc3dd93
ju214425 <ju214425@naver.com>
2025-04-19 14:08:43 +0900

    [level 2] Title: 타겟 넘버, Time: 427.49 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,9 @@
+def solution(numbers, target):
+    
+    if len(numbers) == 0:
+        if target == 0:
+            return 1
+        else: 
+            return 0
+    
+    return solution(numbers[1:], target+numbers[0]) + solution(numbers[1:], target-numbers[0])
